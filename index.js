0c383c4040e57005c77acccc031302d0de6b30fa
github username <github email address>
2020-05-17 10:55:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = [];
+
+function findMatching(drivers, name) {
+  return drivers.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+}
+
+// // Got this to work, but learn wont' accept it, so I'm going to use the solution
+// function fuzzyMatch(drivers, string) {
+//   length = string.length
+//   return drivers.filter(driverName => driverName.slice(0, length).toLowerCase() === string.toLowerCase())
+// }
+
+function fuzzyMatch(source, testString) {
+  return source.filter(possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  )
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
+}
